2898de02ce880d8f6953f08146c0aff7b19d50eb
hex <sasha.nef@yandex.ru>
2023-11-13 13:51:48 +0300

    ВыпадаюДобавляет выпадающее меню на JavaScriptÑ

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,22 @@
-const container = document.querySelector('.before-after-slider');
-document.querySelector('.before-after-slider__slider').addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value}%`);
-})
+const container = document.querySelector(".before-after-slider");
+document
+  .querySelector(".before-after-slider__slider")
+  .addEventListener("input", (e) => {
+    container.style.setProperty("--position", `${e.target.value}%`);
+  });
+let navWrapper = document.querySelector(".navigation__wrapper");
+let navToggle = document.querySelector(".navigation__burger-menu");
+navWrapper.classList.remove("navigation__wrapper--nojs");
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("navigation__wrapper--closed")) {
+    navWrapper.classList.remove("navigation__wrapper--closed");
+    navWrapper.classList.add("navigation__wrapper--opened");
+    navToggle.classList.remove("navigation__burger-menu--closed");
+    navToggle.classList.add("navigation__burger-menu--opened");
+  } else {
+    navWrapper.classList.add("navigation__wrapper--closed");
+    navWrapper.classList.remove("navigation__wrapper--opened");
+    navToggle.classList.remove("navigation__burger-menu--opened");
+    navToggle.classList.add("navigation__burger-menu--closed");
+  }
+});
