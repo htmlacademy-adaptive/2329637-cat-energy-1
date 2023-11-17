9c81b68ae25edc65c64305ad05b16ca3a41d5e6a
Keks <keks@htmlacademy.ru>
2023-11-17 17:08:37 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/burger-menu.js
@@ -0,0 +1,16 @@
+let navWrapper = document.querySelector(".navigation__wrapper");
+let navToggle = document.querySelector(".navigation__burger-menu");
+navWrapper.classList.remove("navigation__wrapper--nojs");
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("navigation__wrapper--closed")) {
+    navWrapper.classList.remove("navigation__wrapper--closed");
+    navWrapper.classList.add("navigation__wrapper--opened");
+    navToggle.classList.remove("navigation__burger-menu--closed");
+    navToggle.classList.add("navigation__burger-menu--opened");
+  } else {
+    navWrapper.classList.add("navigation__wrapper--closed");
+    navWrapper.classList.remove("navigation__wrapper--opened");
+    navToggle.classList.remove("navigation__burger-menu--opened");
+    navToggle.classList.add("navigation__burger-menu--closed");
+  }
+});
