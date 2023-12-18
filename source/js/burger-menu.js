ed7ade7e6299aca15d0cbaeadbf2340fd560d4e6
hex <sasha.nef@yandex.ru>
2023-12-18 09:49:20 +0300

    Доработки

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,8 +1,8 @@
-let navWrapper = document.querySelector(".navigation__wrapper");
-let navToggle = document.querySelector(".navigation__burger-menu");
-navWrapper.classList.remove("navigation__wrapper--nojs");
+let navWrapper = document.querySelector('.navigation__wrapper');
+let navToggle = document.querySelector('.navigation__burger-menu');
+navWrapper.classList.remove('navigation__wrapper--nojs');
 
 navToggle.onclick = function () {
-  navToggle.classList.toggle("navigation__burger-menu--opened");
-  navWrapper.classList.toggle("navigation__wrapper--opened");
+  navToggle.classList.toggle('navigation__burger-menu--opened');
+  navWrapper.classList.toggle('navigation__wrapper--opened');
 };
